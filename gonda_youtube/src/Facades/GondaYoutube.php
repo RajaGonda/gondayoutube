51757eb6e970f07e0f11aeb7bec84dda362866f0
rajagonda <rajagonda@gmail.com>
2019-11-19 18:12:37 +0530

    add code

---
+++ gonda_youtube/src/Facades/GondaYoutube.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Gonda\GondaYoutube\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class GondaYoutube extends Facade {
+
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor() { return 'gondayoutube'; }
+}
---
+++ gonda_youtube/src/GondaYoutubeServiceProvider.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Gonda\GondaYoutube;
+
+use Illuminate\Support\ServiceProvider;
+
+class GondaYoutubeServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $source = realpath(__DIR__ . '/config/gondayoutube.php');
+
+        $this->publishes([$source => config_path('gondayoutube.php')]);
+
+        $this->mergeConfigFrom($source, 'gondayoutube');
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(GondaYoutube::class, function () {
+            return new GondaYoutube(config('youtube.key'));
+        });
+
+        $this->app->alias(GondaYoutube::class, 'gondayoutube');
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [GondaYoutube::class];
+    }
+}
